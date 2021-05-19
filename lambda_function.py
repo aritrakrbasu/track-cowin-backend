4d90f0d25d1f40ee204718780b956e577a61c053
Aritra Basu <43692750+aritrakrbasu@users.noreply.github.com>
2021-05-19 20:18:59 +0530

    fix -service account

--- lambda_function.py
+++ lambda_function.py
@@ -10,13 +10,27 @@ from email.mime.text import MIMEText
 import os
 
 
+serviceAccountdet = {
+  "type": "service_account",
+  "project_id": "track-cowin",
+  "private_key_id": "cdaedb0dddae4d00f0b6028643647d5bc3b0f3cd",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDQkwaWqaGCvtTH\nHBaOITTNvY9yXbwbTQ6CUrtVNNFjGou8hGFIFm7Np9jX63QQfYCxpKDRNHG7WGd7\nCUIBFpVDFVsO2TCZZmeAzBWkReAqZHTPKxl5axVt0NCDeIJ9pIAtr5TbMO8cB6g2\nX47FvRu1lPqFFveH1feMSd6JBC4mRzWvXjSGcx45fPMeb+DGe9eo3RWvVOOQOphY\nx9U4IcOoKE4/mL9OJC72tIkCVIjTswJx1BojjOYKJZjpMhdip8675fKmmTsF5To9\nOSFpkXtnsVhrrql0jZmlMV00fVMfIj2Nb+ML49oAkR6xNLvehGaVjtAzqyKPS05g\nJ6S92VFfAgMBAAECggEAP555Bf+byhuXH1yL7LJoPtExLfs1Y0ZvxISjq2u+6nGw\nacIfQjPFfGlUFRg2gMknNgg3HVjX0AM5HShHv7k5sgqTicXpswCvVXPqrf66T3Pw\nxTgqPwTQtnxY/aEGfHhuuICz8N/OL0iOoHcpP1tdVleoymuk+QPkzB/EBxsFUJse\nZLjQa6FH5ganhhUcQ9ycAS3IJBHAW/KwbamhrAroZpsiMWTib8Q65RvT11xj4Q48\no7vBtX5QUk/c1IWcO4r2PG4Qd+G2LmeZaQ/rv2ayDp0b2ir1T4qcYaPCrp+4XngD\nm/Cmfm3Y60rpLddUhTa2EAJmy+dn60i2CYFKVp0mwQKBgQD7npl/je2DbNceuik+\nnuXexA06ymJVMVLttYhpwEy56IFl9LZPLB9wuBknNDUEyDOxxvZ5UmzCFnx6gq6C\nZVJDO4IuNSSEWy9wfJ+uMWubHwfU2chT1VviHbL0HfNP+yHp6Xzi2/0tUt8svxpm\nwPz+8VuJSj/xhKBWEXAVMc9ScQKBgQDUNJXSI1mvTGxIj9xchs2UUKUDd4fsrlEc\nEoJNqLGpPu2QGdJ7wLT59NvCruXsHelUlipfDYUvcaXM0ZhyXzYEtYhOKffgPCR2\noVxALoMJbUYHIGYmJ6kFuNJ+Q9FBHwcJSXtBW54lvFJ66dLKkZcj8AIdnrqrnaBF\nwgJGeZQozwKBgETpmgaqO1ucSop61+Lzp+dL8IeieC329UxvRG4aIalk+VOQIwIq\nBm5brV/kV4T4w9ezsztlDK5XdD4lXmAOGeqZ+LxpB5hMVQDM0PrnRB5W8FEmWExF\nigWytplPPp0wHYivzule8MciBEeAC14LCv9T4QsHz914wmTwH69eVfaxAoGAERSG\ndpqHzndNQ+3oY74p7+Up0wSc+SzytFq9CDkqy5+YCYA6k+Fn77KIubvQH9gsfrVz\nhaEV9kKkgL7iEVqeg3SFGx1/qRnOKYpFXZkgzPJxr4MpFasdjKtarfURc7dmnpW+\niP0x1oG49dG6OLnNO4RG91FeXw/Z5aN/AWMpzLsCgYEAmNCPSoHos8aIFLXXqS2M\nHNERfsA3LUh9mZv20J8Lmev41JIfDgYOJKqVPSSJPBMSi4MQn8Nn3SfduEFm0xZl\no7tmjJZuzzxZan0FmOnLtwuh0hoiil4OhW9moJGubIKe4X7UxyCZ1NIYFvnWe4Ax\n5SvBQbRV6KPK8NlSOm15W6g=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-hvat5@track-cowin.iam.gserviceaccount.com",
+  "client_id": "108265141722868825983",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hvat5%40track-cowin.iam.gserviceaccount.com"
+}
+
+
 #header to show the request is from a website
 headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         }  
 
 # Use a service account
-cred = credentials.Certificate(os.getenv('serviceAccountdet'))
+cred = credentials.Certificate(serviceAccountdet)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
